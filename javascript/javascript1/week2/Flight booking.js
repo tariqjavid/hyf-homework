d3ad466b5a2d2f76baee267017de5fadf5366412
tariqjavid <khadmies@yahoo.com>
2020-03-05 14:40:25 +0100

    javascript home work week2 has added

---
+++ javascript/javascript1/week2/Flight booking.js
@@ -0,0 +1,20 @@
+//Flight 
+
+function getFullname(fullname1,fullname2,tr)
+
+{
+ 
+    if(tr==1)
+    {
+        return fullname1+" "+fullname2 +" "+"lord";
+    }
+    else
+    {
+    return fullname1+" "+fullname2;
+
+    }
+
+}
+tr=1; //if send here 1, then add Formal fullname otherwise normal
+let res=getFullname("tariq","javid" ,tr);
+console.log(res);

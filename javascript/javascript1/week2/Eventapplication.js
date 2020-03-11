5dd3a56c68926543b9e64b0cac9f67d8ec979348
tariqjavid <khadmies@yahoo.com>
2020-03-11 16:39:18 +0100

    deleted

--- javascript/javascript1/week2/Eventapplication.js
+++
@@ -1,9 +0,0 @@
-
-let currentDate=new Date();
-function getEventWeekday(days) {
-    return new Date(currentDate.getTime() + days*24*60*60*1000);
-}
-
-let days=prompt("please Enter event days up to now")
-var newDate = getEventWeekday(days);
-console.log("The event will be held on this date:",newDate);

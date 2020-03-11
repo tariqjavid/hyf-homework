24a07565820c8601612d456491a6a776b1d4152f
tariqjavid <khadmies@yahoo.com>
2020-03-11 16:40:27 +0100

    new files added

---
+++ javascript/javascript1/week2/Weatherwear.js
@@ -0,0 +1,38 @@
+
+<<<<<<< HEAD
+function youCreateThisFunctionName(clothType)
+
+{
+
+ if(clothType=="shirt")
+{
+ return "please wear shirts and sweater becuase there is cold outside othewise you will get throut infection";
+}
+if (clothType=="tshirt") {
+
+ return "Enjoy the summer,oooh my God";
+ 
+}   
+}
+
+let res=youCreateThisFunctionName("tshirt");
+=======
+
+function youCreateThisFunctionName(clothType)
+
+   {
+
+    if(clothType=="shirt")
+{
+    return "please wear shirts and sweater becuase there is cold outside othewise you will get throut infection";
+}
+if (clothType=="tshirt") {
+
+    return "Enjoy the summer,oooh my God";
+    
+}   
+   }
+
+   let res=youCreateThisFunctionName("tshirt");
+>>>>>>> master
+console.log(res);

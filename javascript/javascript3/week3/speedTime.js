5ea707372a897cdee86f657147d207ca54787fbd
tariqjavid <55512927+tariqjavid@users.noreply.github.com>
2020-04-30 21:33:40 +0200

    done
    done

--- javascript/javascript3/week3/speedTime.js
+++ javascript/javascript3/week3/speedTime.js
@@ -6,17 +6,16 @@ const travelInformation = {
     destinationDistance: 432,//km 
   };
 
-  function notThisFunctionName(travelInformation){
+  function taravlingCalculator(travelInformation){
 
   
     let dec = travelInformation.destinationDistance/travelInformation.speed;
 
-    let  hr = parseInt(Number(dec));
-    var mn = Math.round((Number(dec)-hr) * 60);
+    let  hr = parseInt((dec));
+    var mn = Math.round(((dec)-hr) * 60);
     return ("hours"+" "+ hr + " "+" Minuts"+" " + mn);
     
   }
   
-  const travelTime = notThisFunctionName(travelInformation);
+  const travelTime = taravlingCalculator(travelInformation);
   console.log(travelTime);
-   

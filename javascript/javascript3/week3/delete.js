dd2f687c8bdb39d6c62b5b26ba97e1e328e4acd2
tariqjavid <55512927+tariqjavid@users.noreply.github.com>
2020-04-30 16:19:38 +0200

    Update delete.js
    ok done

--- javascript/javascript3/week3/delete.js
+++ javascript/javascript3/week3/delete.js
@@ -1,11 +1,10 @@
+
 //Item array removal
 const names = ['Peter', 'Ahmad', 'Yana', 'Kristina', 'Rasmus', 'Samuel', 'Katrine', 'Tala'];
 const nameToRemove = 'Ahmad';
 
-for (var i = 0; i < names.length; i++) {
-    if (names[i]===nameToRemove) {
-      names.splice(i, 1);  
-    }
-}      
-document.write(names); 
+        const index=names.indexOf(nameToRemove);
+        names.splice(index, 1);
+        console.log(names)
+        
 

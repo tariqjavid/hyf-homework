b16df542e42aefbe395d3ab7f2899e27f2c31a9b
tariqjavid <khadmies@yahoo.com>
2020-08-06 23:48:48 +0200

    changes in week1

--- react/week1/src/App.js
+++ react/week1/src/App.js
@@ -1,25 +1,11 @@
 import React, { Component } from "react";
 import "./index.css";
 class todList extends Component {
-          state = {
-                    key1: { color: "green" },
-                    key2: { color: "red" },
-                    key3: { color: "blue" },
-          };
-          render() {
+                  render() {
                     return (
                               <div className="wraper">
-                                        <h1>Todo List</h1>
-                                        <p style={this.state.key1}>
-                                                  {this.props.tList.key1}
-                                        </p>
-                                        <p style={this.state.key2}>
-                                                  {" "}
-                                                  {this.props.tList.key2}
-                                        </p>
-                                        <p style={this.state.key3}>
-                                                  {this.props.tList.key3}
-                                        </p>
+                              <h1>Todo List</h1>
+                              <ul>{this.props.tList.map((val,index)=><li style={{color:`rgb(${index}55,${index},0)`}}>{val.description} </li>)}</ul>
                               </div>
                     );
           }
--- react/week1/src/Todolist.js
+++ react/week1/src/Todolist.js
@@ -1,6 +1,7 @@
-const todoList = {
-          key1: "Get out of bed, Wed Sep 13 2017",
-          key2: "Brush teeth, Thu Sep 14 2017",
-          key3: "Eat breakfast, Fri Sep 15 2017",
-};
+const todoList = [
+            {"description": "Get out of bed, Wed Sep 13 2017"},
+            { "description":"Brush teeth, Thu Sep 14 2017"},
+            { "description":"Eat breakfast, Fri Sep 15 2017"}
+]
+
 export default todoList;
